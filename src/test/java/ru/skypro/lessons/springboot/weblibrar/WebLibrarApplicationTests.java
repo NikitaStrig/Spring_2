6cc619b0852f413e2f391069fba153cce9385372
nikitastrigunkov <demonkurt1995@gmail.com>
2023-04-30 16:29:23 +0300

    Begin

---
+++ src/test/java/ru/skypro/lessons/springboot/weblibrar/WebLibrarApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.weblibrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLibrarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
